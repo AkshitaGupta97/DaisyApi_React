d60b023bbc6cc6e11292353e8df1689ff88fc75c
Akshita Gupta <gungunakshita37gupta973@gmail.com>
2025-09-19 19:52:03 +0530

    using axios and api to fetch

--- src/Components/CoinTable/CoinTable.jsx
+++ src/Components/CoinTable/CoinTable.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { fetchCoinData } from '../../Services/fetchCoinData'
 
 function CoinTable() {
+
+    const [page, setPage] = useState()
+
+
+
   return (
     <div>
       
