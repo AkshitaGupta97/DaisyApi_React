a0717c09fa48574280d85f5ed6242ebdfd7fb03b
Akshita Gupta <gungunakshita37gupta973@gmail.com>
2025-09-23 22:15:46 +0530

    created Error Boundary for resolving errors occurred in routes

--- src/ErrorBoundary/ErrorBoundary.jsx
+++ src/ErrorBoundary/ErrorBoundary.jsx
@@ -5,10 +5,12 @@ import {ErrorBoundary} from 'react-error-boundary'
 
 function ErrorBoundary({error, resetErrorBoundary}) {
   return (
-    <div role='alert' className='alert alert-error'> {/* alert alert-error -> this classname is from daisyUi */}
-        <p>Something went wrong :</p>
-        <div>{error?.message}</div>
-        <button onClick={resetErrorBoundary}>Try Again</button>
+    <div className='h-[100vh] flex justify-center items-center'>
+        <div role='alert' className='alert alert-error'> {/* alert alert-error -> this classname is from daisyUi */}
+            <p>Something went wrong :</p>
+            <div>{error?.message}</div>
+            <button onClick={resetErrorBoundary}>Try Again</button>
+        </div>
     </div>
   )
 }
