1a19b96d88d284e22a5fe5e0e3977a3ca360702e
Akshita Gupta <gungunakshita37gupta973@gmail.com>
2025-09-23 21:50:09 +0530

    created ErrorBoundary

---
+++ src/ErrorBoundary/ErrorBoundary.jsx
@@ -0,0 +1,25 @@
+import { Children } from 'react'
+import {ErrorBoundary} from 'react-error-boundary'
+
+// this page is created to resolve when the error occurs, while fetching the data.
+
+function ErrorBoundary({error, resetErrorBoundary}) {
+  return (
+    <div role='alert' className='alert alert-error'> {/* alert alert-error -> this classname is from daisyUi */}
+        <p>Something went wrong :</p>
+        <div>{error?.message}</div>
+        <button onClick={resetErrorBoundary}>Try Again</button>
+    </div>
+  )
+}
+
+export default function CustomErrorBoundary({ children }){
+    return (
+        <ErrorBoundary
+            FallbackComponent={ErrorBoundary}
+            onReset={() => window.location.reload()}
+        >
+            {children}
+        </ErrorBoundary>
+    )
+}
--- src/Routing/Routing.jsx
+++ src/Routing/Routing.jsx
@@ -6,31 +6,34 @@ import Home from '../pages/Home'
 import CoinDetailsPage from '../pages/CoinDetailsPage'
  */
 import Layout from '../pages/Layout'
-import  { Facebook } from 'react-content-loader'  // or you can make your own custom loaders from react-content-loader
+import { Facebook } from 'react-content-loader'  // or you can make your own custom loaders from react-content-loader
+import { ErrorBoundary } from 'react-error-boundary'
 
 const Home = lazy(() => import('../pages/Home'))
 const CoinDetailsPage = lazy(() => import('../pages/CoinDetailsPage'))
 
 function Routing() {
   return (
-    <Routes>
-      <Route path='/' element={<Layout />}>
+    <ErrorBoundary>
+      <Routes>
+        <Route path='/' element={<Layout />}>
 
-        <Route index element={
-          <Suspense fallback={<Facebook />}>  {/** This  Facebook content will show page appearance like facebook when the content is not loaded */}
-            <Home />
-          </Suspense>
-        } />  {/* index - is same as path="/", just to make things simple */}
+          <Route index element={
+            <Suspense fallback={<Facebook />}>  {/** This  Facebook content will show page appearance like facebook when the content is not loaded */}
+              <Home />
+            </Suspense>
+          } />  {/* index - is same as path="/", just to make things simple */}
 
-        <Route path='/details/:coinId' element={
-          <Suspense fallback={<Facebook />}>          {/*Suspense -> we are wrapping the CoinDetails page in Suspense because we want to show something till any item is downloaded. */}
-            <CoinDetailsPage />
-          </Suspense>
-        } />
+          <Route path='/details/:coinId' element={
+            <Suspense fallback={<Facebook />}>          {/*Suspense -> we are wrapping the CoinDetails page in Suspense because we want to show something till any item is downloaded. */}
+              <CoinDetailsPage />
+            </Suspense>
+          } />
 
-      </Route>
+        </Route>
 
-    </Routes>
+      </Routes>
+    </ErrorBoundary>
   )
 }
 
