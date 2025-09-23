d4fba4563dfdb22967879f959c786242db0d815a
Akshita Gupta <gungunakshita37gupta973@gmail.com>
2025-09-23 14:32:16 +0530

    lazy loading in routing

--- src/Routing/Routing.jsx
+++ src/Routing/Routing.jsx
@@ -1,18 +1,24 @@
 
-import React from 'react'
-import {Route, Routes} from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
+import { lazy, Lazy } from 'react'
+/*
 import Home from '../pages/Home'
 import CoinDetailsPage from '../pages/CoinDetailsPage'
+ */
 import Layout from '../pages/Layout'
+
+const Home = lazy(() => import('../pages/Home'))
+const CoinDetailsPage = lazy(() => import('../pages/CoinDetailsPage'))
 
 function Routing() {
   return (
     <Routes>
-        <Route path='/' element={<Layout/>}>
-          <Route index element={<Home />} />  {/* index - is same as path="/", just to make things simple */}
-          <Route path='/details/:coinId' element={<CoinDetailsPage />} />
-        </Route>
-        
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Home />} />  {/* index - is same as path="/", just to make things simple */}
+        <Route path='/details/:coinId' element={<CoinDetailsPage />} />
+
+      </Route>
+
     </Routes>
   )
 }
